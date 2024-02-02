bc1f1acc220f952f1cbd99bde353f5e12eb328f9
abdulwalii <abdulwalii652@gmail.com>
2024-02-02 19:29:24 +0500

    dwdw

--- src/sockets/gameSocket.js
+++ src/sockets/gameSocket.js
@@ -10,6 +10,14 @@ import { getUser } from "../controller/authController.js";
 export const gameSocket = (io) => {
     io.on("connection", (socket) => {
 
+        io.engine.on("connection_error", (err) => {
+            console.log(err.req);      // the request object
+            console.log(err.code);     // the error code, for example 1
+            console.log(err.message);  // the error message, for example "Session ID unknown"
+            console.log(err.context);  // some additional error context
+          });
+
+
         // create room
         socket.on("createRoom", async (arg) => {
             const roomId = generateString(8);
